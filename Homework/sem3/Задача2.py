6d465b9244b51f4732b0f6030ecefbc45b81e52c
Alex <alexey.kalyagin@ya.ru>
2022-10-06 21:55:32 +0300

    задача2 сем3

--- Homework/sem3/Задача2.py
+++ Homework/sem3/Задача2.py
@@ -8,22 +8,16 @@ n = int(input('Введите значение размера списка N: ')
 listN = []
 k = n-1
 sum1 = 0
-#sum2 = 0
-#sum3 = 0
-#sum4 = 0 
-#sum5 = 0
 
 for elem in range(n):
     listN.append(random.randint(0,n))
 
 print(listN) 
-#print (k)
+
 for j in range(len(listN)):
     
-    if j < k:
+    if j <= k:
         sum1= listN[j]*listN[k]
         k -=1
         print(sum1)
        
-
-#print(sum1)
